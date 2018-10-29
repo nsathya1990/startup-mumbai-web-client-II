26d2d2639e1800e28c762cabf005b42493b04d4d
Sathyapriyan <nsathya1990@gmail.com>
2018-10-29 19:12:30 +0530

    updated server.js

--- server.js
+++ server.js
@@ -13,7 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/ngStartupMumbai/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
+app.listen(process.env.PORT || 8080);
